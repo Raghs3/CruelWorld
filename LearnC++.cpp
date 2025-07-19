65434bebdce224acba114c8a9a6205c2b070d9ff
Raghs3 <tinku.minku2803@gmail.com>
2025-07-19 12:16:45 +0530

    data types

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -15,13 +15,39 @@ using namespace std;
 //     return 0;
 // }
 
+// int main() {
+//     // int is datatype // 4 bytes
+//     // int x = 10;
+//     // // long
+//     // long x = 15;
+//     // cin >> x;
+//     // // long long
+//     // long long x = 1500000000;
+// 
+//     // float - 4 byte, double - 8 byte
+//     float x = 5.6;
+//     double y = 6;
+//     cout << "Value of y: " << y;
+// 
+//     return 0;
+// }
+
 int main() {
-    // int is datatype // 4 bytes
-    int x = 10;
-    // long
-    long x = 15;
-    cin >> x;
-    // long long
-    long long x = 1500000000;
+//     string and getline
+//     string s1;
+//     string s2;  // After space, new string
+// 
+//     cin >> s1 >> s2;  //  ONLY Hey comes
+//     cout << s1 << " " << s2;
+
+    // string str;
+    // getline(cin, str);  // now takes entire line but doesn't get next line (after pressing enter)
+    // cout << str;
+
+    // char can store all characters (256)
+    char ch = 'g';  // string in double quotes and takes more space
+    // cin >> ch;
+    cout << ch;
+
     return 0;
 }
