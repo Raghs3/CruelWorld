af9bfb7ecd0d1711b2bd609f09a2331d6e52a661
Raghs3 <tinku.minku2803@gmail.com>
2025-10-04 14:28:05 +0530

    N-Forest

---
+++ patterns.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print1(int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main() {  // in rounds won't code this part
+    int n;
+    cin >> n;
+    print1(n);
+}
